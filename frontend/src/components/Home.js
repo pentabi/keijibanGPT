63c219192ab5d5164e7410ccbd570d0baef25f45
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-09-09 13:08:39 +0900

    [init] react

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,28 @@
+
+
+const Home = (props) => {
+  return (
+    <div>
+      Hello
+      {/* <title>掲示板GPT</title>
+      <link rel="stylesheet" href="homepage.css"></link>
+      <link rel="stylesheet" href="mediaqueries.css"></link>
+      <body>
+        <div class="header">
+          <div class="logo">掲示板GPT</div>
+          <div class="button-container">
+            <a href="create.html"><button class="gray-button">スレを立てる</button></a>
+          </div>
+        </div>
+
+        <div class="threads">
+          <div class="thread-text">スレッド</div>
+          <a href="thread.html"><div class="gray-box">明日晴れてほしいスレ</div></a>
+          <a href="thread.html"><div class="gray-box">明日曇りがいいスレ</div></a>
+        </div>
+      </body> */}
+    </div>
+  )
+}
+
+export default Home;
