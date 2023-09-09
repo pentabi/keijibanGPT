960560bb405796749b3ed5d029f5311ec155aada
pentabi <tabitosatoh@gmail.com>
2023-09-09 16:32:31 +0900

    gitignore and new branch

--- backend/select.py
+++ backend/select.py
@@ -3,14 +3,11 @@ import sqlite3
 while True:
     con = sqlite3.connect('./sample.db')
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS TEST(id INTEGER PRIMARY KEY AUTOINCREMENT,name text)")
-    sql = 'INSERT INTO TEST(name) values (?)'
-    name = str(input("名前を入力:"))
-    cur.execute(sql, name)
+    cur.execute("CREATE TABLE IF NOT EXISTS TEST(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+    name = input("名前を入力:")  # Removed unnecessary str() conversion
+    cur.execute("INSERT INTO TEST(name) VALUES (?)", (name,))
     cur.execute("SELECT * FROM TEST")
     con.commit()
     for row in cur:
         print(str(row[0]) + "," + str(row[1]))
     con.close()
-
-
