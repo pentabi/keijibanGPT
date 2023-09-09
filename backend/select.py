d9cbd921a6676a484fa902cfe07a3445d50441ea
pentabi <tabitosatoh@gmail.com>
2023-09-09 17:09:40 +0900

    get the last row of the database

--- backend/select.py
+++ backend/select.py
@@ -6,8 +6,17 @@ while True:
     cur.execute("CREATE TABLE IF NOT EXISTS TEST(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
     name = input("名前を入力:")  # Removed unnecessary str() conversion
     cur.execute("INSERT INTO TEST(name) VALUES (?)", (name,))
-    cur.execute("SELECT * FROM TEST")
     con.commit()
-    for row in cur:
-        print(str(row[0]) + "," + str(row[1]))
+
+    # Retrieve the last row from the table
+    cur.execute("SELECT * FROM TEST ORDER BY id DESC LIMIT 1")
+    last_row = cur.fetchone()
+
+    if last_row:
+        print("Last Row:")
+        print("ID:", last_row[0])
+        print("Name:", last_row[1])
+    else:
+        print("No rows in the table yet.")
+
     con.close()
