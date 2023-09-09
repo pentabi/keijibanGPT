3e99d4d83a06046404d4d5e2964d03246c0c721c
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-09-09 13:55:09 +0900

    [fix] route bag #3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import "./components/Home";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      Hello from App.js
     </div>
   );
 }
