d9ee005f0d46eae621039d4c2a4b1f28e012da9f
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-09-09 17:37:11 +0900

    [add] add react component at thread

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,15 @@
-import "./home.css"
+import "./home.css";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "../routes/routes";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import ApiClient from "../api/ApiClient";
 
 const data = {
   "thread_list": [
@@ -54,6 +62,71 @@ const data = {
   ]
 }
 
+const Popup = (props) => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const apiClient = ApiClient.instance;
+  const navigate = useNavigate();
+
+  const handleCreate = () => {
+    console.log("name: " + name);
+    if (name === "") {
+      setError(true);
+      return;
+    }
+    const data = { "thread_add": { "thread_name": name } };
+    console.log(data);
+    apiClient
+      .post("create", data)
+      .then((res) => {
+        console.log(res);
+        navigate(ROUTES.HOME);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("failed to create");
+      });
+    setName("");
+    props.setCreate(false);
+  };
+
+  return (
+    <div>
+      <Dialog open={props.create} onClose={() => props.setCreate(true)}>
+        <DialogTitle>新しいスレッド</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            会話をはじめよう
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="新しいスレッドの名前"
+            type="text"
+            fullWidth
+            variant="standard"
+            error={error}
+            onChange={
+              (e) => {
+                setName(e.target.value);
+                if (e.target.value === "") {
+                  setError(true);
+                } else {
+                  setError(false);
+                }
+              }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button color="inherit" onClick={() => { props.setCreate(false); setName("") }}>Cancel</Button>
+          <Button color="inherit" onClick={handleCreate}>Make a new Thread</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
 const Home = (props) => {
   const navigate = useNavigate();
   const [create, setCreate] = useState(false);
@@ -61,15 +134,18 @@ const Home = (props) => {
   return (
     <div>
       <title>掲示板GPT</title>
+      <Popup create={create} setCreate={setCreate} />
       <body>
         <div class="header">
           <div class="logo">掲示板GPT</div>
           <div class="button-container">
             <button class="gray-button"
-              onClick={(e) => {
-                navigate(ROUTES.THREAD);
+              onClick={() => {
+                setCreate(true);
               }}
-            >スレを立てる</button>
+            >
+              スレを立てる
+            </button>
           </div>
         </div>
 
@@ -81,7 +157,14 @@ const Home = (props) => {
                 <p>
                   <button class="gray-box"
                     onClick={() => {
-                      navigate(ROUTES.THREAD);
+                      const id = d["thread"]["thread_id"];
+                      navigate(`${ROUTES.THREAD}/`, {
+                        state: {
+                          "thread_id": d["thread"]["thread_id"],
+                          "thread_name": d["thread"]["thread_name"],
+                          "comment_list": d["thread"]["comment_list"]
+                        }
+                      });
                     }}
                   >
                     {d["thread"]["thread_name"]}
--- frontend/src/components/Thread.js
+++ frontend/src/components/Thread.js
@@ -1,42 +1,98 @@
 import './thread.css';
+import { useNavigate, useRoute, useParams, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { ROUTES } from "../routes/routes";
+import TextField from '@mui/material/TextField';
+import ApiClient from '../api/ApiClient';
 
-const Thread = (props) => {
-    return (
+const Thread = (props, route) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { thread_id, thread_name, comment_list } = location.state;
+  const [name, setName] = useState();
+  const [text, setText] = useState();
+  const [error, setError] = useState(false);
+  const apiClient = ApiClient.instance;
+
+  const handlePost = () => {
+    if (text === "") {
+      setError(true);
+      return;
+    }
+    if (name === "") {
+      setName("匿名");
+    }
+    setError(true);
+    const data = { "comment_add": { "thread_id": thread_id, "content": text, "name": name } };
+    apiClient
+      .post(`thread/${thread_id}/`, data)
+      .then((res) => {
+        console.log(res);
+        apiClient
+          .get(`thread/${thread_id}/`)
+          .then((res) => {
+            /// ここにコードを追加！！！
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("failed to post");
+      });
+    setText("");
+  };
+
+  return (
     <div>
-        <head>
-        <title>明日晴れるといいなスレ</title>
+      <head>
+        <title>{thread_name}</title>
         <link rel="stylesheet" href="style.css"></link>
-        </head>
-        <body>
+      </head>
+      <body>
         <div class="container">
-            {/* 戻るを押せるようにする */}
-            <div class="to-left">戻る</div>
-            <div class="title">
-            明日晴れるといいなスレ
-            </div>
+          <button class="to-left" onClick={() => { navigate(ROUTES.HOME) }}>戻る</button>
+          <div class="title">
+            {thread_name}
+          </div>
 
-            <div class="list">
-            <p>
-            1: 名前 : <span class="content">晴れるといいな</span><span class="name"></span>
-            </p>
-            <p>
-            2: 名前 :<span class="content">まじそれな</span><span class="name"></span>
-            </p>
-            <p>
-            3: 名前 : <span class="content">てるてる坊主まじ必須なんだが</span><span class="name"></span>
-            </p>
-            </div>
+          <div class="list">
+            {
+              comment_list.map((d) => {
+                return (
+                  <div>
+                    <p>
+                      {d["comment"]["comment_id"]}: {d["comment"]["name"]}
+                    </p>
+                    <p>
+                      {d["comment"]["content"]}
+                    </p>
+                  </div>
 
-            <div class="post-section">
-            <input type="text" class="input-field" placeholder="名前"></input>
-            <textarea class="input-field" placeholder="書き込む内容"></textarea>
-            <button class="post-button">書き込む</button>
-            </div>
+                )
+              })
+            }
+          </div>
+          <div class="post-section">
+            <TextField id="outlined-basic" label="名前" variant="outlined" onChange={(e) => { setName(e.target.value) }}></TextField>
+            <TextField id="outlined-basic" fullWidth label="書き込み" variant="outlined" multiline rows={2} error={error}
+              onChange={(e) => {
+                setName(e.target.value);
+                if (e.target.value === "") {
+                  setError(true);
+                } else {
+                  setError(false);
+                }
+              }}
+            ></TextField>
+            <button class="post-button" onClick={handlePost}>書き込む</button>
+          </div>
         </div>
 
-        </body>
+      </body>
     </div>
-    );
+  );
 };
 
 export default Thread;
