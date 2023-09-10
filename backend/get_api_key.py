c8e064e049cb2b447f2f67120778ef5501dcb4a0
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-09-10 15:39:05 +0900

    [fix] change readme

--- backend/get_api_key.py
+++ backend/get_api_key.py
@@ -4,8 +4,8 @@ def get_api_key():
     try:
         f = open("api_key.txt", "r")
     except OSError as e:
-        print("1. Please make \"api_key.txt\" file in this directory.")
-        print("2. Put your \"ChatGPT API KEY\" in this file.")
+        print("1. Please make \"api_key.txt\" file in the backend directory.")
+        print("2. Put your \"ChatGPT API KEY\" in the file.")
         sys.exit(0)
     else:
         content = f.readlines()
